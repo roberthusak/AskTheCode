ae27dba8c7058bc146828b97ce96a9ca8c2b1de2
husak <husak@DESKTOP-513TJ6M>
2016-07-18 09:19:11 +0200

    Create a basic layout of a standalone GUI application

---
+++ samples/StandaloneGui/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AskTheCode.ControlFlowGraphs.Cli.Tests;
+using Microsoft.CodeAnalysis;
+
+namespace StandaloneGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        public Document OpenedDocument { get; private set; }
+
+        public async Task OpenDocument(Document document)
+        {
+            this.OpenedDocument = document;
+            var sourceText = await this.OpenedDocument.GetTextAsync();
+            this.code.Text = sourceText.ToString();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var workspace = SampleCSharpWorkspaceProvider.MethodSampleClass();
+            var document = workspace.CurrentSolution.Projects.Single().Documents.Single();
+            await this.OpenDocument(document);
+        }
+
+        private void DisplayCfg_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+    }
+}
---
+++ samples/StandaloneGui/MsaglGraphPresenter.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Msagl.WpfGraphControl;
+
+namespace StandaloneGui
+{
+    /// <summary>
+    /// Interaction logic for MsaglGraphPresenter.xaml
+    /// </summary>
+    public partial class MsaglGraphPresenter : UserControl
+    {
+        public static readonly DependencyProperty GraphViewerProperty = DependencyProperty.Register(
+                "GraphViewer",
+                typeof(GraphViewer),
+                typeof(MsaglGraphPresenter),
+                new PropertyMetadata(new GraphViewer()));
+
+        public MsaglGraphPresenter()
+        {
+            this.InitializeComponent();
+
+            this.GraphViewer.LayoutEditingEnabled = false;
+            this.GraphViewer.BindToPanel(this.graphViewerPanel);
+        }
+
+        public GraphViewer GraphViewer
+        {
+            get { return (GraphViewer)this.GetValue(GraphViewerProperty); }
+        }
+    }
+}
