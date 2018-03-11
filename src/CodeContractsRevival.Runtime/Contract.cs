d9f91905f3716f2d355f5766101870f2190850f7
Robert Husák <robert@husak.cloud>
2018-03-11 09:37:49 +0100

    Add project to mock subset of Code Contracts

---
+++ src/CodeContractsRevival.Runtime/Contract.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CodeContractsRevival.Runtime
+{
+    public static class Contract
+    {
+        [Conditional("DEBUG")]
+        public static void Assert(bool condition)
+        {
+            Debug.Assert(condition);
+        }
+
+        [Conditional("DEBUG")]
+        public static void Requires(bool condition)
+        {
+            Debug.Assert(condition);
+        }
+
+        public static void Requires<TException>(bool condition) where TException : Exception
+        {
+            if (!condition)
+            {
+                throw ExceptionHelper<TException>.Create();
+            }
+        }
+
+        public static void Requires<TException>(bool condition, string userMessage) where TException : Exception
+        {
+            if (!condition)
+            {
+                throw ExceptionHelper<TException>.CreateWithMessage(userMessage);
+            }
+        }
+
+        public static bool ForAll<T>(IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            return collection.All(predicate);
+        }
+    }
+}
