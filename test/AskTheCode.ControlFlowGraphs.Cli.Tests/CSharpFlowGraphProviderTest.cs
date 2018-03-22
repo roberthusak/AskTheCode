640ee8f85a8441074426848dcf51071df15e1ada
Robert Husák <robert@husak.cloud>
2018-03-22 11:52:19 +0100

    Add automated CFG generation tests
    Upgrade the CLI CFG test project to MSTest v2 in the process.

---
+++ test/AskTheCode.ControlFlowGraphs.Cli.Tests/CSharpFlowGraphProviderTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AskTheCode.ControlFlowGraphs.Cli.Tests
+{
+    [TestClass]
+    public class CSharpFlowGraphProviderTest
+    {
+        private const string InputsBaseDir = @"..\..\..\inputs\csharp\";
+
+        [TestMethod]
+        [CSharpSolutionMethodTestData(InputsBaseDir + @"Sample\Sample.csproj")]
+        public void SampleProjectMethodsFlowGraphCreationDoesntThrow(Solution solution, MethodLocation method)
+        {
+            ConstructControlFlowGraph(solution, method);
+        }
+
+        [TestMethod]
+        [CSharpSolutionMethodTestData(InputsBaseDir + @"EvaluationTests\EvaluationTests.csproj")]
+        public void EvaluationProjectMethodsFlowGraphCreationDoesntThrow(Solution solution, MethodLocation method)
+        {
+            ConstructControlFlowGraph(solution, method);
+        }
+
+        private static void ConstructControlFlowGraph(Solution solution, MethodLocation method)
+        {
+            var flowGraphProvider = new CSharpFlowGraphProvider(solution);
+            var result = flowGraphProvider.GetFlowGraphAsync(method).Result;
+
+            Assert.AreNotEqual(null, result);
+        }
+    }
+}
---
+++ test/AskTheCode.ControlFlowGraphs.Cli.Tests/CSharpSolutionMethodTestDataAttribute.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AskTheCode.ControlFlowGraphs.Cli.Tests
+{
+    public class CSharpSolutionMethodTestDataAttribute : Attribute, ITestDataSource
+    {
+        private readonly MSBuildWorkspace csharpWorkspace;
+        private readonly Project project;
+
+        public CSharpSolutionMethodTestDataAttribute(string projectLocation)
+        {
+            this.csharpWorkspace = MSBuildWorkspace.Create();
+            this.project = this.csharpWorkspace.OpenProjectAsync(projectLocation).Result;
+        }
+
+        public string ClassNameFilter { get; set; }
+
+        public string MethodNameFilter { get; set; }
+
+        /// <summary>
+        /// Recursively retrieve all the methods from the loaded project.
+        /// </summary>
+        /// <param name="methodInfo">The test method the data are generated for.</param>
+        /// <returns>Enumeration of two-item arrays consisting of an instance of <see cref="Solution"/>
+        /// and an instance of <see cref="MethodLocation"/>.</returns>
+        public IEnumerable<object[]> GetData(MethodInfo methodInfo)
+        {
+            var compilation = this.project.GetCompilationAsync().Result;
+
+            var namespaceQueue = new Queue<INamespaceSymbol>();
+            var typeQueue = new Queue<ITypeSymbol>();
+
+            namespaceQueue.Enqueue(compilation.Assembly.GlobalNamespace);
+
+            while (namespaceQueue.Count > 0)
+            {
+                var ns = namespaceQueue.Dequeue();
+
+                foreach (var nestedNs in ns.GetNamespaceMembers())
+                {
+                    namespaceQueue.Enqueue(nestedNs);
+                }
+
+                foreach (var type in ns.GetTypeMembers())
+                {
+                    typeQueue.Enqueue(type);
+                }
+            }
+
+            while (typeQueue.Count > 0)
+            {
+                var type = typeQueue.Dequeue();
+
+                foreach (var nestedType in type.GetTypeMembers())
+                {
+                    typeQueue.Enqueue(nestedType);
+                }
+
+                if (this.ClassNameFilter == null || type.Name == this.ClassNameFilter)
+                {
+                    foreach (var method in type.GetMembers().OfType<IMethodSymbol>())
+                    {
+                        if (this.MethodNameFilter == null || method.Name == this.MethodNameFilter)
+                        {
+                            var location = new MethodLocation(method);
+                            yield return new object[] { this.csharpWorkspace.CurrentSolution, location };
+                        }
+                    }
+                }
+            }
+        }
+
+        public string GetDisplayName(MethodInfo methodInfo, object[] data)
+        {
+            return ((MethodLocation)data[1]).Method.ToDisplayString();
+        }
+    }
+}
