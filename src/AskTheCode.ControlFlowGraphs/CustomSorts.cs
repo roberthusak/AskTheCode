3c6af037305be010e765252ce34cc889a3580989
Robert Husák <robert@husak.cloud>
2018-03-30 20:27:24 +0200

    Add notion of classes and fields to ControlFlowGraphs

---
+++ src/AskTheCode.ControlFlowGraphs/CustomSorts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AskTheCode.SmtLibStandard;
+
+namespace AskTheCode.ControlFlowGraphs
+{
+    public static class CustomSorts
+    {
+        public static Sort Reference { get; } = Sort.CreateCustom("Reference");
+    }
+}
---
+++ src/AskTheCode.ControlFlowGraphs/TypeSystem/IFieldDefinition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AskTheCode.SmtLibStandard;
+
+namespace AskTheCode.ControlFlowGraphs.TypeSystem
+{
+    public interface IFieldDefinition
+    {
+        Sort Sort { get; }
+
+        IClassDefinition ReferencedClass { get; }
+    }
+
+    public static class IFieldDefinitionExtensions
+    {
+        public static bool IsReference(this IFieldDefinition field) => field.ReferencedClass != null;
+    }
+}
