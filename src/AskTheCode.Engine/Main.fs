80fffb0f1c5e8acffd66d157f933e888f4695532
Robert Husák <robert@husak.cloud>
2019-07-15 23:13:39 +0200

    Put samples to separate file

--- src/AskTheCode.Engine/Main.fs
+++ src/AskTheCode.Engine/Main.fs
@@ -1,250 +1,17 @@
 module AskTheCode.Program
 
-open System.Linq
-open System.Collections.Generic
-open Microsoft.CodeAnalysis
-open Microsoft.CodeAnalysis.CSharp
-open Microsoft.CodeAnalysis.CSharp.Syntax
-open Microsoft.CodeAnalysis.FlowAnalysis
-
 open AskTheCode
 open AskTheCode.Smt
 open AskTheCode.Cfg
 open AskTheCode.SymbolicExecution
-open AskTheCode.Cli
 open AskTheCode.Gui
 open System.Diagnostics
-
-let swapNodeSource = @"
-using System.Diagnostics;
-
-public class Node
-{
-    public int value;
-    public Node next;
-
-    public Node SwapNode()
-    {
-        Node result = this;
-        if (this.next != null)
-        {
-            if (this.value > this.next.value)
-            {
-                Node t = this.next;
-                this.next = t.next;
-                t.next = this;
-                result = t;
-            }
-        }
-
-        return result;
-    }
-}"
-
-let swapNodeSource2 = @"
-using System.Diagnostics;
-
-public class Node
-{
-    public int value;
-    public Node next;
-
-    public int SwapNode()
-    {
-        Node result = this;
-        if (this.next != null)
-        {
-            if (this.value > this.next.value)
-            {
-                Node t = this.next;
-                this.next = t.next;
-                t.next = this;
-                result = t;
-            }
-
-            if (this.value > this.next.value)
-            {
-                return 1;
-            }
-        }
-
-        return 0;
-    }
-}"
-
-let nodeMaxSource = @"
-public class Node
-{
-    public int value;
-    public Node next;
-
-    public static int Max(Node a, Node b)
-    {
-        Node m;
-        if (a.value >= b.value)
-        {
-            m = a;
-        }
-        else
-        {
-            m = b;
-        }
-
-        if (m.value < a.value || m.value < b.value)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-}"
-
-let nodeMaxSource2 = @"
-public class Node
-{
-    public int value;
-    public Node next;
-
-    public static int Max(Node m, Node b)
-    {
-        if (b.value > m.value)
-        {
-            b = b.next;
-            m = b;
-        }
-
-        if (m.value < b.value)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-}"
-
-let degreeCountingSource = @"
-using System;
-
-public class DegreeCounting
-{
-    public class Node
-    {
-        public Node next;
-        public Node pointsTo;
-
-        public int index;
-        public int inDegree;
-    }
-
-    public int Compute(Node first)
-    {
-        Node n = first;
-        int index = 0;
-        while (n != null)
-        {
-            n.inDegree = 0;
-            n.index = index;
-
-            n = n.next;
-            index = index + 1;
-        }
-
-        n = first;
-        while (n != null)
-        {
-            if (n.pointsTo != null)
-            {
-                n.pointsTo.inDegree = n.pointsTo.inDegree + 1;
-            }
-
-            n = n.next;
-        }
-
-        n = first;
-        while (n != null)
-        {
-            if (n.index > 1 && n.inDegree > n.index)
-            {
-                return 1;
-            }
-
-            n = n.next;
-        }
-
-        return 0;
-    }
-}"
-
-let absoluteValueSource = @"
-public class C
-{
-    public static int AbsoluteValue(int x)
-    {
-        int y;
-        if (x >= 0)
-        {
-            y = x;
-        }
-        else
-        {
-            y = -x;
-        }
-
-        if (y < 0)
-        {
-            return 1;
-        }
-
-        return 0;
-    }
-}"
-
-let absoluteValue2source = @"
-public class C
-{
-    public static int AbsoluteValue(int x)
-    {
-        x = x + 1;
-        if (x >= 0)
-        {
-        }
-        else
-        {
-            x = -x;
-        }
-
-        if (x < 0)
-        {
-            return 1;
-        }
-
-        return 0;
-    }
-}"
-
-let sourceToCfg (source:string) =
-    let parseOptions = CSharpParseOptions.Default.WithFeatures([ new KeyValuePair<string, string>("flow-analysis", "flow-analysis") ])
-    let syntaxTree = CSharpSyntaxTree.ParseText(source, parseOptions)
-    let reference = MetadataReference.CreateFromFile(typeof<System.Object>.Assembly.Location)
-    let compilation = CSharpCompilation.Create("test", [ syntaxTree ], [ reference ])
-    let syntaxNode = syntaxTree.GetRoot().DescendantNodes().OfType<MethodDeclarationSyntax>().Single()
-    let roslynCfg = ControlFlowGraph.Create(syntaxNode, compilation.GetSemanticModel(syntaxTree, true))
-    Cfg.convertCfg roslynCfg
 
 [<EntryPoint>]
 let main args =
 
-    //let (cfg, target) = (sourceToCfg swapNodeSource, NodeId 4)
-    //let (cfg, target) = (sourceToCfg swapNodeSource2, NodeId 5)
-    //let (cfg, target) = (sourceToCfg nodeMaxSource, NodeId 6)
-    //let (cfg, target) = (sourceToCfg nodeMaxSource2, NodeId 4)
-    let (cfg, target) = (sourceToCfg degreeCountingSource, NodeId 13)
-    //let (cfg, target) = (sourceToCfg absoluteValueSource, NodeId 5)
-    //let (cfg, target) = (sourceToCfg absoluteValue2source, NodeId 4)
+    let sample = Samples.degreeCounting()
+    let cfg = sample.Cfg
 
     printfn "%s" <| Graph.print cfg
     cfg |> Msagl.convertCfg |> Msagl.displayGraph
@@ -270,8 +37,8 @@ let main args =
 
     let condFn = (Exploration.solverCondFn solver)
     //let condFn = Z3.stackCondFn ctx
-    let res = Exploration.run condFn ArrayHeap.functions cfg (Graph.node cfg target)
-    //let res = Exploration.mergeRun condFn ArrayHeap.functions cfg (Graph.node cfg target)
+    let res = Exploration.run condFn ArrayHeap.functions cfg (Graph.node cfg sample.TargetNode)
+    //let res = Exploration.mergeRun condFn ArrayHeap.functions cfg (Graph.node cfg sample.TargetNode)
 
     wholeWatch.Stop()
 
---
+++ src/AskTheCode.Engine/Samples.fs
@@ -0,0 +1,242 @@
+﻿module AskTheCode.Samples
+
+open System.Linq
+open System.Collections.Generic
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp
+open Microsoft.CodeAnalysis.CSharp.Syntax
+open Microsoft.CodeAnalysis.FlowAnalysis
+
+open AskTheCode
+open AskTheCode.Cfg
+open AskTheCode.Cli
+
+let swapNodeSource = @"
+using System.Diagnostics;
+
+public class Node
+{
+    public int value;
+    public Node next;
+
+    public Node SwapNode()
+    {
+        Node result = this;
+        if (this.next != null)
+        {
+            if (this.value > this.next.value)
+            {
+                Node t = this.next;
+                this.next = t.next;
+                t.next = this;
+                result = t;
+            }
+        }
+
+        return result;
+    }
+}"
+
+let swapNodeSource2 = @"
+using System.Diagnostics;
+
+public class Node
+{
+    public int value;
+    public Node next;
+
+    public int SwapNode()
+    {
+        Node result = this;
+        if (this.next != null)
+        {
+            if (this.value > this.next.value)
+            {
+                Node t = this.next;
+                this.next = t.next;
+                t.next = this;
+                result = t;
+            }
+
+            if (this.value > this.next.value)
+            {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+}"
+
+let nodeMaxSource = @"
+public class Node
+{
+    public int value;
+    public Node next;
+
+    public static int Max(Node a, Node b)
+    {
+        Node m;
+        if (a.value >= b.value)
+        {
+            m = a;
+        }
+        else
+        {
+            m = b;
+        }
+
+        if (m.value < a.value || m.value < b.value)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}"
+
+let nodeMaxSource2 = @"
+public class Node
+{
+    public int value;
+    public Node next;
+
+    public static int Max(Node m, Node b)
+    {
+        if (b.value > m.value)
+        {
+            b = b.next;
+            m = b;
+        }
+
+        if (m.value < b.value)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}"
+
+let degreeCountingSource = @"
+using System;
+
+public class DegreeCounting
+{
+    public class Node
+    {
+        public Node next;
+        public Node pointsTo;
+
+        public int index;
+        public int inDegree;
+    }
+
+    public int Compute(Node first)
+    {
+        Node n = first;
+        int index = 0;
+        while (n != null)
+        {
+            n.inDegree = 0;
+            n.index = index;
+
+            n = n.next;
+            index = index + 1;
+        }
+
+        n = first;
+        while (n != null)
+        {
+            if (n.pointsTo != null)
+            {
+                n.pointsTo.inDegree = n.pointsTo.inDegree + 1;
+            }
+
+            n = n.next;
+        }
+
+        n = first;
+        while (n != null)
+        {
+            if (n.index > 1 && n.inDegree > n.index)
+            {
+                return 1;
+            }
+
+            n = n.next;
+        }
+
+        return 0;
+    }
+}"
+
+let absoluteValueSource = @"
+public class C
+{
+    public static int AbsoluteValue(int x)
+    {
+        int y;
+        if (x >= 0)
+        {
+            y = x;
+        }
+        else
+        {
+            y = -x;
+        }
+
+        if (y < 0)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+}"
+
+let absoluteValue2source = @"
+public class C
+{
+    public static int AbsoluteValue(int x)
+    {
+        x = x + 1;
+        if (x >= 0)
+        {
+        }
+        else
+        {
+            x = -x;
+        }
+
+        if (x < 0)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+}"
+
+let sourceToCfg (source:string) =
+    let parseOptions = CSharpParseOptions.Default.WithFeatures([ new KeyValuePair<string, string>("flow-analysis", "flow-analysis") ])
+    let syntaxTree = CSharpSyntaxTree.ParseText(source, parseOptions)
+    let reference = MetadataReference.CreateFromFile(typeof<System.Object>.Assembly.Location)
+    let compilation = CSharpCompilation.Create("test", [ syntaxTree ], [ reference ])
+    let syntaxNode = syntaxTree.GetRoot().DescendantNodes().OfType<MethodDeclarationSyntax>().Single()
+    let roslynCfg = ControlFlowGraph.Create(syntaxNode, compilation.GetSemanticModel(syntaxTree, true))
+    Cfg.convertCfg roslynCfg
+
+type Sample = { Name: string; Cfg: Graph; TargetNode: NodeId; IsValid: bool }
+
+let nodeSwap1 () = { Name = "Node swap #1"; Cfg = sourceToCfg swapNodeSource; TargetNode = NodeId 4; IsValid = false }
+let nodeSwap2 () = { Name = "Node swap #2"; Cfg = sourceToCfg swapNodeSource2; TargetNode = NodeId 5; IsValid = true }
+let nodeMax1 () = { Name = "Node max #1"; Cfg = sourceToCfg nodeMaxSource; TargetNode = NodeId 6; IsValid = true }
+let nodeMax2 () = { Name = "Node max #2"; Cfg = sourceToCfg nodeMaxSource2; TargetNode = NodeId 4; IsValid = false }
+let degreeCounting () = { Name = "Degree counting"; Cfg = sourceToCfg degreeCountingSource; TargetNode = NodeId 13; IsValid = false }
+let absoluteValue1 () = { Name = "Absolute value #1"; Cfg = sourceToCfg absoluteValueSource; TargetNode = NodeId 5; IsValid = true }
+let absoluteValue2 () = { Name = "Absolute value #2"; Cfg = sourceToCfg absoluteValue2source; TargetNode = NodeId 4; IsValid = true }
