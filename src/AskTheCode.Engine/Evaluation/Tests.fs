ccda250d502debd73424eb1edf07fe827b463c65
Robert Husák <robert@husak.cloud>
2019-09-07 09:53:05 +0200

    Repeat test runs and compute average time

--- src/AskTheCode.Engine/Evaluation/Tests.fs
+++ src/AskTheCode.Engine/Evaluation/Tests.fs
@@ -14,17 +14,22 @@ let setSolve idSet id = Set.contains id idSet
 let efficiency sample algorithm =
     let trgNode = Graph.node sample.Cfg sample.TargetNode
     let doSolve = setSolve <| Set.ofList sample.LoopStarts
+    let run () = algorithm.Function doSolve sample.Cfg trgNode
 
-    let wholeWatch = Stopwatch.StartNew()
-    let res = algorithm.Function doSolve sample.Cfg trgNode
-    wholeWatch.Stop()
+    let res = run()
 
     match (res, sample.IsValid) with
     | (h::t, true) -> printfn "ERROR: The counterexample shouldn't have been found (%s, %s)." sample.Name algorithm.Name
     | ([], false) -> printfn "ERROR: The counterexample should have been found (%s, %s)." sample.Name algorithm.Name
     | _ -> ()
 
-    wholeWatch.ElapsedMilliseconds
+    let repeats :int64 = 10L
+    let wholeWatch = Stopwatch.StartNew()
+    for i in 1L..repeats do
+        run() |> ignore
+    wholeWatch.Stop()
+
+    wholeWatch.ElapsedMilliseconds / repeats
 
 let compare discipline printer samples algorithms =
     for algorithm in algorithms do
