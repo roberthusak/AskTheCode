db80332e463456b58268fe4df074a8248d5e7ee9
Robert Husák <robert@husak.cloud>
2021-05-15 00:29:24 +0200

    Fix VSIX compilation and inclusion of referenced assemblies

--- src/AskTheCode.Vsix/Properties/AssemblyInfo.cs
+++ src/AskTheCode.Vsix/Properties/AssemblyInfo.cs
@@ -7,19 +7,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-[assembly: ProvideBindingRedirection(
-    AssemblyName = "System.Reactive.Core",
-    PublicKeyToken = "94bc3704cddfc263",
-    Culture = "neutral",
-    OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "3.0.3000.0",
-    NewVersion = "3.0.3000.0")]
-
-[assembly: ProvideBindingRedirection(
-    AssemblyName = "System.Reactive.Interfaces",
-    PublicKeyToken = "94bc3704cddfc263",
-    Culture = "neutral",
-    OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "3.0.1000.0",
-    NewVersion = "3.0.1000.0")]
